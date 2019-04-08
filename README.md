569434f9e84a2339f0480e9db48c6e69d86ff575
Anthony Garay <agaray1@jhu.edu>
2019-04-08 02:59:04 -0400

    Flutter set up, pubspec created, audiplayer plugin added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # localup
+
+# installing Flutter
+https://flutter.dev/docs/get-started/install
+
+# installing audioplayer 0.5.2
+https://pub.dartlang.org/packages/audioplayer#-installing-tab-
+
+
---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <audioplayer/AudioplayerPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AudioplayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"AudioplayerPlugin"]];
+}
+
+@end

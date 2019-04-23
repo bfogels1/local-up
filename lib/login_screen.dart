f054ca407abad2ced13c568f79a52da87e032da1
Camila <ccamach5@jhu.edu>
2019-04-22 21:52:39 -0400

    Listener or Musician Start Page

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -13,6 +13,107 @@ class _LoginScreenState extends State<LoginScreen>
     super.initState();
   }
 
+  Widget WhoPage() {
+    return new Container(
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        color: Colors.redAccent[200],
+        image: DecorationImage(
+          colorFilter: new ColorFilter.mode(
+              Colors.black.withOpacity(0.1), BlendMode.dstATop),
+          image: AssetImage('assets/images/mic.jpeg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: new Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 200.0),
+            child: Center(
+                child: Image.asset('assets/images/localup_white.png')
+            ),
+          ),
+          new Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 150.0),
+            alignment: Alignment.center,
+            child: new Row(
+              children: <Widget>[
+                new Expanded(
+                  child: new OutlineButton(
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0)),
+                    color: Colors.redAccent,
+                    highlightedBorderColor: Colors.white,
+                    onPressed: () => gotoHomePage(),
+                    child: new Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20.0,
+                        horizontal: 20.0,
+                      ),
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          new Expanded(
+                            child: Text(
+                              "LISTENER",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 30.0),
+            alignment: Alignment.center,
+            child: new Row(
+              children: <Widget>[
+                new Expanded(
+                  child: new FlatButton(
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0)),
+                    color: Colors.white,
+                    onPressed: () => gotoLogin(),
+                    child: new Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20.0,
+                        horizontal: 20.0,
+                      ),
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          new Expanded(
+                            child: Text(
+                              "MUSICIAN",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.redAccent,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
   Widget HomePage() {
     return new Container(
       height: MediaQuery.of(context).size.height,
@@ -698,6 +799,15 @@ class _LoginScreenState extends State<LoginScreen>
     );
   }
 
+  gotoHomePage() {
+    //controller_minus1To0.reverse(from: 0.0);
+    _controller.animateToPage(
+      1,
+      duration: Duration(milliseconds: 800),
+      curve: Curves.bounceOut,
+    );
+  }
+
   gotoLogin() {
     //controller_0To1.forward(from: 0.0);
     _controller.animateToPage(
@@ -716,7 +826,8 @@ class _LoginScreenState extends State<LoginScreen>
     );
   }
 
-  PageController _controller = new PageController(initialPage: 1, viewportFraction: 1.0);
+  PageController _controller = new PageController(initialPage: 3,
+      viewportFraction: 1.0);
 
   @override
   Widget build(BuildContext context) {
@@ -725,7 +836,7 @@ class _LoginScreenState extends State<LoginScreen>
         child: PageView(
           controller: _controller,
           physics: new AlwaysScrollableScrollPhysics(),
-          children: <Widget>[LoginPage(), HomePage(), SignupPage()],
+          children: <Widget>[LoginPage(), HomePage(), SignupPage(), WhoPage()],
           scrollDirection: Axis.horizontal,
         ));
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:local_up/main.dart';
+import 'package:localup/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

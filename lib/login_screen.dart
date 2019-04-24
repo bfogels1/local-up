1ac176300ebee619e589fa1c8a9bd84779a7e704
Camila <ccamach5@jhu.edu>
2019-04-24 14:20:45 -0400

    Start on Listener Set-Up

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -692,7 +692,7 @@ class _LoginScreenState extends State<LoginScreen>
                           borderRadius: new BorderRadius.circular(30.0),
                         ),
                         color: Colors.redAccent,
-                        onPressed: () => {},
+                        onPressed: () => gotoSetup(),
                         child: new Container(
                           padding: const EdgeInsets.symmetric(
                             vertical: 20.0,
@@ -725,6 +725,86 @@ class _LoginScreenState extends State<LoginScreen>
     );
   }
 
+  // Set Up Pages
+  // Preferences -> : Music Taste (genres with slider) -> Live Show Radius ->
+  // Live Show Price -> Artist Matching (traits with slider)
+
+  Widget setupPage() {
+    return new Container(
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        image: DecorationImage(
+          colorFilter: new ColorFilter.mode(
+              Colors.white.withOpacity(0.05), BlendMode.dstATop),
+          image: AssetImage('assets/images/mic.jpeg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Icon(Icons.menu),
+          backgroundColor: Color(0x00000000),
+          elevation: 0.0,
+        ),
+        body: Center(
+          child: new Column(
+            children: <Widget>[
+              // LOGO
+              Container(
+                padding: EdgeInsets.only(top: 50.0),
+                child: Center(
+                    child: Image.asset('assets/images/localup_color.png')
+                ),
+              ),
+
+              // CONTINUE
+              new Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 50.0),
+                alignment: Alignment.center,
+                child: new Row(
+                  children: <Widget>[
+                    new Expanded(
+                      child: new FlatButton(
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(30.0),
+                        ),
+                        color: Colors.redAccent,
+                        onPressed: () => {},
+                        child: new Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 20.0,
+                            horizontal: 20.0,
+                          ),
+                          child: new Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              new Expanded(
+                                child: Text(
+                                  "CONTINUE",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   gotoLogin() {
     //controller_0To1.forward(from: 0.0);
     _controller.animateToPage(
@@ -743,6 +823,15 @@ class _LoginScreenState extends State<LoginScreen>
     );
   }
 
+  gotoSetup() {
+    //controller_minus1To0.reverse(from: 0.0);
+    _controller.animateToPage(
+      3,
+      duration: Duration(milliseconds: 800),
+      curve: Curves.bounceOut,
+    );
+  }
+
   PageController _controller = new PageController(initialPage: 1,
       viewportFraction: 1.0);
 
@@ -753,7 +842,7 @@ class _LoginScreenState extends State<LoginScreen>
         child: PageView(
           controller: _controller,
           physics: new AlwaysScrollableScrollPhysics(),
-          children: <Widget>[loginPage(), startPage(), signupPage()],
+          children: <Widget>[loginPage(), startPage(), signupPage(), setupPage()],
           scrollDirection: Axis.horizontal,
         ));
   }

664886094723121c648d37470f94a8d3c0eadd8b
Anthony Garay <agaray1@jhu.edu>
2019-04-25 16:28:13 -0400

    AudioPlayer Page Working!

---
+++ lib/widgets/carousel.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+
+class CarouselApp extends StatefulWidget {
+  @override
+  _CarouselAppState createState() => new _CarouselAppState();
+}
+
+class _CarouselAppState extends State<CarouselApp> {
+
+  Widget _buildCarousel(BuildContext context, int carouselIndex) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Text('Carousel $carouselIndex'),
+        SizedBox(
+          // you may want to use an aspect ratio here for tablet support
+          height: 200.0,
+          child: PageView.builder(
+            // store this controller in a State to save the carousel scroll position
+            controller: PageController(viewportFraction: 0.8),
+            itemBuilder: (BuildContext context, int itemIndex) {
+              return _buildCarouselItem(context, carouselIndex, itemIndex);
+            },
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildCarouselItem(BuildContext context, int carouselIndex, int itemIndex) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 4.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+        ),
+      ),
+    );
+  }
+}
